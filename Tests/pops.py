65e70a1b32f341e473f85cf319f75a4a109b556c
wnj00524 <68168066+wnj00524@users.noreply.github.com>
2021-05-30 21:34:08 +0100

    Added locations barebones.

--- Tests/pops.py
+++ Tests/pops.py
@@ -1,44 +1,78 @@
 def new_ind(ID = -1, type=1):
     if type == 1:
-        i1 = individual(ID, {"hunger": 0, "rest": 0}, None)
+        i1 = individual(ID, {"hunger": 0, "rest": 0}, None, memory({}))
 
     return i1
 
+class location(dict):
+    def __init__(self, ID = -1, present = [], type = None, description = "So empty."):
+        self.ID = ID
+        self.type = type
+        self.present = present
+        self.description = description
 
 
+class population:
 
-class pop_c:
-    ID = -1
-    members = []
+    def __init__(self, ID = -1, members = [], locations = {}):
+        self.ID = ID
+        self.members = members
+        self.locations = locations
+        self.add_location("the ether","The starting ether.", "ether")
+
+
+    def return_def_loc(self, title, description, type):
+        new_ID = len(self.members)
+        new_loc = location()
+        new_loc.ID = new_ID
+        new_loc.description = description
+        return new_loc
+
+    def add_location(self, title = "default", description = "So empty.", type=None):
+        self.locations[title] = self.return_def_loc(title, description, type)
+
+
     def tick(self, debug=False):
         for member in self.members:
             #\print(member)
-            member.tick()
+            member.individual_tick()
             if debug:
                 print(member.ID, " is currently ", member.state, "\nTired = ", member.Needs["rest"], " \nHunger = ", member.Needs["hunger"])
 
 
+class memory:
+    def __init__(self, locations = {"food": [], "rest": []}):
+        locations = locations
 
 class individual:
-    def __init__(self, ID, Needs, State):
+    hour = 0
+    def __init__(self, ID, Needs, State, Memory, type = 1):
         self.ID = ID
-        self.Needs = Needs
+        self.needs = Needs
         self.state = State
+        self.memory = Memory
+        self.type = type
 
-    def tick(self=None):
-        if self.Needs["hunger"] > 5:
-            self.state = "food_search"
-        else:
-            self.state = "resting"
+    def individual_tick(self=None):
+        self.hour += 1
+        if self.type == 1:
+            #set the state
+            if self.needs["hunger"] > 5:
+                self.state = "food_search"
+            else:
+                self.state = "resting"
 
-        if self.state == "resting" and self.Needs["rest"] > 0:
-            self.Needs["rest"] -= 1
-        elif self.state == "food_search":
-            #do food logic
-            self.Needs["hunger"] -= 3
-        else:
+            #carry out actions
 
-            self.Needs["rest"] += 1
-            self.Needs["hunger"] += 1
+
+            #update the needs
+            if self.state == "resting" and self.needs["rest"] > 0:
+                self.needs["rest"] -= 1
+            elif self.state == "food_search":
+                #do food logic
+                self.needs["hunger"] -= 3
+            else:
+                self.needs["rest"] += 1
+                self.needs["hunger"] += 1
 
 
--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from Tests import com
 import Tests.pops as pops
 
 if __name__ == '__main__':
-    main_p = pops.pop_c()
+    main_p = pops.population()
     i1 = pops.new_ind()
     i1.ID = 1
     i2 = pops.new_ind()
     i2.ID = 2
-
     main_p.members.append(i1)
     main_p.members.append(i2)
+    print(main_p.members[0].needs["hunger"])
     for a in range(0, 100):
-        main_p.tick(True)
+        main_p.tick()
+    print(main_p.members[0].needs["hunger"])
+    print(main_p.locations["the ether"].description)
+
+
 
 
 
