eb243f56ab4f0f9f05d07e291616be96deef4f1c
wnj00524 <68168066+wnj00524@users.noreply.github.com>
2021-05-30 23:42:11 +0100

    Add knowledge of locations; added movement between locations.

--- Tests/pops.py
+++ Tests/pops.py
@@ -1,8 +1,10 @@
 def new_ind(ID = -1, type=1):
     if type == 1:
-        i1 = individual(ID, {"hunger": 0, "rest": 0}, None, memory({}))
+        i1 = individual(ID, {"hunger": 0, "rest": 0}, None, memory())
 
     return i1
+
+
 
 class location(dict):
     def __init__(self, ID = -1, present = [], type = None, description = "So empty."):
@@ -19,10 +21,12 @@ class population:
         self.members = members
         self.locations = locations
         self.add_location("the ether","The starting ether.", "ether")
+        self.add_location("eating place", "Where we eat!", "eating")
+        self.add_location("resting place", "The place in which we rest.", "resting")
 
 
     def return_def_loc(self, title, description, type):
-        new_ID = len(self.members)
+        new_ID = len(self.locations)
         new_loc = location()
         new_loc.ID = new_ID
         new_loc.description = description
@@ -32,26 +36,42 @@ class population:
         self.locations[title] = self.return_def_loc(title, description, type)
 
 
+    def add_individual(self, type = 1):
+        new_ID = len(self.members)
+        temp_individual = new_ind(new_ID,type)
+        self.members.append(temp_individual)
+        self.locations["the ether"].present.append(self.members[new_ID])
+
+
+
     def tick(self, debug=False):
         for member in self.members:
             #\print(member)
+            temp_key = member.location_key[:]
             member.individual_tick()
+            #has the location key changed? if so, actually move the individual!
+            if temp_key != member.location_key:
+                #print(temp_key, " is the same as ", member.location_key)
+                self.locations[temp_key].present.pop()
+                self.locations[member.location_key].present.append(member)
             if debug:
-                print(member.ID, " is currently ", member.state, "\nTired = ", member.Needs["rest"], " \nHunger = ", member.Needs["hunger"])
+                print(member.ID, " is currently ", member.state, "\nTired = ", member.needs["rest"], " \nHunger = ", member.needs["hunger"], "\nNow at ", member.location_key)
 
 
-class memory:
-    def __init__(self, locations = {"food": [], "rest": []}):
-        locations = locations
+class memory(dict):
+    def __init__(self, locations = {"food": ["eating place"], "rest": ["resting place"]}):
+        self["main"] = locations
+
 
 class individual:
     hour = 0
-    def __init__(self, ID, Needs, State, Memory, type = 1):
+    def __init__(self, ID, Needs, State, memory = memory(), type = 1, location_key = "the ether"):
         self.ID = ID
         self.needs = Needs
         self.state = State
-        self.memory = Memory
+        self.memory = memory
         self.type = type
+        self.location_key = location_key
 
     def individual_tick(self=None):
         self.hour += 1
@@ -59,8 +79,10 @@ class individual:
             #set the state
             if self.needs["hunger"] > 5:
                 self.state = "food_search"
+                self.location_key = self.memory["main"]["food"][0]
             else:
                 self.state = "resting"
+                self.location_key = self.memory["main"]["rest"][0]
 
             #carry out actions
 
@@ -74,5 +96,6 @@ class individual:
             else:
                 self.needs["rest"] += 1
                 self.needs["hunger"] += 1
+        return self.location_key
 
 
--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import Tests.pops as pops
 
 if __name__ == '__main__':
     main_p = pops.population()
-    i1 = pops.new_ind()
-    i1.ID = 1
-    i2 = pops.new_ind()
-    i2.ID = 2
-    main_p.members.append(i1)
-    main_p.members.append(i2)
-    print(main_p.members[0].needs["hunger"])
+    main_p.add_individual(1)
+    main_p.add_individual(1)
+    main_p.add_individual(1)
+    main_p.add_individual(1)
+    main_p.add_individual(1)
+
     for a in range(0, 100):
         main_p.tick()
     print(main_p.members[0].needs["hunger"])
-    print(main_p.locations["the ether"].description)
+
 
 
 
